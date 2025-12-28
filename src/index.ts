c1c82e6b76110929506d7ff80a2482af717346f4
Fadi Obaji <fadi.obaji@gmail.com>
2025-12-28 17:27:48 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { generateRandomString } from "./utils/string-generator"
+import { generateRandomString } from "./utils/string"
+import { generateSecureRandomString } from "./utils/uuid"
+import { generateRandomNumber } from "./utils/number"
 
 const main = () => {
+    console.log("Random String:")   
     console.log(generateRandomString(10))
+    console.log("--------------------------------")
+
+    console.log("Secure Random String:")
+    console.log(generateSecureRandomString(10))
+    console.log("--------------------------------")
+
+    console.log("Random Number:")
+    console.log(generateRandomNumber())
+    console.log("--------------------------------")
 }
 
 main()
---
+++ src/utils/number.ts
@@ -0,0 +1,6 @@
+/**
+ * Generate a random number between 0 and 1
+ */
+export const generateRandomNumber = (): number => {
+    return Math.random()
+}
